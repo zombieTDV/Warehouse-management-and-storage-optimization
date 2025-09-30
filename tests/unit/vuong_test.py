25cacaa9d65fc04510805b1db21bb9dc67ba340b
zombieTDV <zombietdv2701@gmail.com>
2025-09-30 17:39:21 +0700

    -Thêm test

--- tests/unit/vuong_test.py
+++ tests/unit/vuong_test.py
@@ -1,4 +1,32 @@
-from casey.cli.menu import run
+from casey.cli.menu import sum, subtract
+
+# from casey.cli.menu import *'
+
+
 def test_sample():
-    if 1 == 2:
-        run()
+    pass
+
+
+# Unit tests
+def test_sum_positive_numbers():
+    assert sum(2, 3) == 5, "sum(2, 3) phải bằng 5"
+
+
+def test_sum_negative_numbers():
+    assert sum(-1, -4) == -5, "sum(-1, -4) phải bằng -5"
+
+
+def test_sum_mixed_numbers():
+    assert sum(-2, 5) == 3, "sum(-2, 5) phải bằng 3"
+
+
+def test_subtract_positive_numbers():
+    assert subtract(10, 4) == 6, "subtract(10, 4) phải bằng 6"
+
+
+def test_subtract_result_negative():
+    assert subtract(3, 5) == -2, "subtract(3, 5) phải bằng -2"
+
+
+def test_subtract_with_zero():
+    assert subtract(7, 0) == 7, "subtract(7, 0) phải bằng 7"
